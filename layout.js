826e884bed3c029f70e9c85ad7287d397b78d5f5
abdallawae <63566233+abdallawae@users.noreply.github.com>
2026-09-07 23:50:38 +0300

    Delete layout.js

--- layout.js
+++
@@ -1,3 +0,0 @@
-import "./style.css";
-export const metadata={title:"MIS Career Hub",description:"منصة طلاب نظم المعلومات"};
-export default function RootLayout({children}){return <html lang="ar" dir="rtl"><body>{children}</body></html>}
